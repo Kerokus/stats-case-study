edca32d7c7ccc0f6b28353454b825f19cd6f182f
Kerokus <levenebb@gmail.com>
2024-07-09 18:10:14 -0400

    Initial file and folder structure

---
+++ src/template.py
@@ -0,0 +1,12 @@
+import numpy as np
+import scipy.stats as stats
+import math
+import pandas as pd
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+from itertools import combinations
+
+
+"""
+This is just a template file to populate the folder in github.
+"""
